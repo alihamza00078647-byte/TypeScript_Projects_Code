d49c54b752f77c5f267cdc4fac2c796805db150a
alihamza00078647-byte <alihamza00078647@gmail.com>
2026-07-27 08:25:09 +0500

    TS Code

---
+++ 2.Lecture inferencing and innotations/src/index.ts
@@ -0,0 +1,16 @@
+// inferencing (Auto determine)
+
+let drink = "Chai peyo";
+
+// drink = 0   Not assign due to a number
+
+let chanelName = "MeraChannel";
+// chanelName = "Nawa Naam"   Ok Ok hai
+console.log(chanelName);
+
+
+// annotations (Explicitly Declare)
+let num :Number = 20;
+console.log(20+1);
+
+
